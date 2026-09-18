5431c2763ff8dca9ea72cb76c0726f4e7d6126ca
AjiPratama53 <aji.nanindra27@gmail.com>
2026-09-18 13:34:30 +0700

    Fix: vercel deployment error

--- service/src/app.ts
+++ service/src/app.ts
@@ -18,9 +18,11 @@ app.use("/v1", packagesRouter);
 app.use("/v1", paymentsRouter);
 app.get("/health", (_req, res) => res.sendStatus(200));
 
-const port = process.env.PORT ?? 3000;
-app.listen(port, () =>
-  console.log(`Listening on http://localhost:${port}/v1/`),
-);
+if (require.main === module) {
+  const port = process.env.PORT ?? 3000;
+  app.listen(port, () =>
+    console.log(`Listening on http://localhost:${port}/v1/`),
+  );
+}
 
 export default app;
